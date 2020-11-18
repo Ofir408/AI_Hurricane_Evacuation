04478c51ae8586f6aff07cb0699a49e9525da356
Ofir Ben Shoham <Ofir408@gmail.com>
2020-11-18 11:04:13 +0200

    Added an option to insert config path as a main argument

--- Main.py
+++ Main.py
@@ -1,10 +1,11 @@
 import os
+import sys
 
 from Runner import Runner
 from configuration_reader.ConfigurationReader import ConfigurationReader
 
-config_path = os.path.abspath(os.curdir) + "\initial_configurations\input.txt"
 configuration_reader = ConfigurationReader()
+config_path = sys.argv[1] if len(sys.argv) > 1 else os.path.abspath(os.curdir) + "\initial_configurations\input.txt"
 config = configuration_reader.read_configuration(config_path)
 runner = Runner()
 runner.run(config)

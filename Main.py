873e22a65c2a1805a1c46224c2003b1868031497
Ofir Ben Shoham <Ofir408@gmail.com>
2020-11-18 10:57:24 +0200

    Changed default configuration path

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ import os
 from Runner import Runner
 from configuration_reader.ConfigurationReader import ConfigurationReader
 
-config_path = os.getcwd() + "\initial_configurations\input.txt"
+config_path = os.path.abspath(os.curdir) + "\initial_configurations\input.txt"
 configuration_reader = ConfigurationReader()
 config = configuration_reader.read_configuration(config_path)
 runner = Runner()
